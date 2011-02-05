87f2fcc8cf863a3665a9e63ce69b59285e45dc14
Andreas J. Koenig <andreas.koenig.7os6VVqR@franz.ak.mind.de>
2011-02-05 12:27:22 +0100

    provide a callback on Recent::news and use it in fsck

--- lib/File/Rsync/Mirror/Recent.pm
+++ lib/File/Rsync/Mirror/Recent.pm
@@ -243,11 +243,16 @@ Test this with:
        -remote pause.perl.org::authors/RECENT.recent
        -verbose
 
-Note: all parameters that can be passed to
+All parameters that can be passed to
 File:Rsync:Mirror:Recentfile::recent_events() can also be specified
 here.
 
-Note: all data are kept in memory
+One additional option is supported. If C<$Options{callback}> is
+specified, it must be a subref. This sub is called whenever one chunk
+of events is found. The first argument to the callback is a reference
+to the currently accumulated array of events.
+
+Note: all data are kept in memory.
 
 =cut
 
@@ -297,6 +302,9 @@ sub news {
         }
         $before = $res->[-1]{epoch};
         $before = $opt{before} if $opt{before} && _bigfloatlt($opt{before},$before);
+        if (my $sub = $opt{callback}) {
+            $sub->($ret);
+        }
     }
     $ret;
 }
